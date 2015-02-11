c61e815b82a77b0684a8fec0af80e18236983b95
Natasha Halfin <natasha_halfin@yahoo.com>
2015-02-11 17:34:01 -0600

    added data wrangled dataframes and updated markdown file

--- 02 Data Wrangling/dataWrangling.R
+++ 02 Data Wrangling/dataWrangling.R
@@ -1,2 +1,4 @@
 txCaCompanyData <- companyData %>% select(RANK, STATE, COMPANY, INDUSTRY)%>% filter(STATE %in% c("TX", "CA"))
 industryGrowthData <- companyData %>% select(INDUSTRY, cummean(REVENUE), cummean(GROWTH))%>% group_by(INDUSTRY)
+stateGrowth <- companyData %>% select(STATE, CITY, cummax(GROWTH), cummax(REVENUE))%>% group_by(STATE)%<% arrange(STATE)
+cityGrowth <- companyData %>% select(CITY, cummin(GROWTH), cummin(REVENUE))%>% group_by(CITY) %>% arrange(desc(GROWTH))
