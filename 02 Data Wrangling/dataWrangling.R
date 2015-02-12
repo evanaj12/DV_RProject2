90630115e62d89eb7b3144463a8945226030f9db
Evan Johnston <evanjohnston@wireless-10-147-26-137.public.utexas.edu>
2015-02-12 16:56:26 -0600

    function from kz and nh

--- 02 Data Wrangling/dataWrangling.R
+++ 02 Data Wrangling/dataWrangling.R
@@ -1,4 +1,4 @@
 txCaCompanyData <- companyData %>% select(RANK, STATE, COMPANY, INDUSTRY)%>% filter(STATE %in% c("TX", "CA"))
-industryGrowthData <- companyData %>% select(INDUSTRY, cummean(REVENUE), cummean(GROWTH))%>% group_by(INDUSTRY)
-stateGrowth <- companyData %>% select(STATE, CITY, cummax(GROWTH), cummax(REVENUE))%>% group_by(STATE)%>% arrange(STATE)
-cityGrowth <- companyData %>% select(CITY, cummin(GROWTH), cummin(REVENUE))%>% group_by(CITY) %>% arrange(desc(GROWTH))
+industryGrowthData <- companyData %>% group_by(INDUSTRY) %>% summarise (revMean = mean(REVENUE), groMean = mean(GROWTH))
+stateGrowth <- companyData %>% group_by(STATE) %>% summarise (groMax = cummax(GROWTH), revMax = cummax(REVENUE))
+cityGrowth <- companyData %>% group_by (CITY) %>% summarise (groMin = cummin(GROWTH), revMin = cummin(REVENUE))%>% arrange(desc(GROWTH))
