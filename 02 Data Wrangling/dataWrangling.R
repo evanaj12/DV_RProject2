456060acdd88cf17195668e6da83cb5dac06513b
Natasha Halfin <natasha_halfin@yahoo.com>
2015-02-12 19:13:59 -0600

    updated database in Oracle and changed calls to reflect new column names. Updated markdown file.

--- 02 Data Wrangling/dataWrangling.R
+++ 02 Data Wrangling/dataWrangling.R
@@ -1,4 +1,5 @@
 txCaCompanyData <- companyData %>% select(RANK, STATE, COMPANY, INDUSTRY)%>% filter(STATE %in% c("TX", "CA"))
-industryGrowthData <- companyData %>% group_by(INDUSTRY) %>% summarise (revMean = mean(REVENUE), groMean = mean(GROWTH))
-stateGrowth <- companyData %>% group_by(STATE) %>% summarise (groMax = cummax(GROWTH), revMax = cummax(REVENUE))
-cityGrowth <- companyData %>% group_by (CITY) %>% summarise (groMin = cummin(GROWTH), revMin = cummin(REVENUE))%>% arrange(desc(GROWTH))
+industryGrowthData <- companyData %>% group_by(INDUSTRY) %>% summarise (revMean = mean(REVENUE), groMean = mean(PERCENTAGEGROWTH))
+stateGrowth <- companyData %>% group_by(STATE) %>% summarise (groMax = max(PERCENTAGEGROWTH), revMax = max(REVENUE))
+cityGrowth <- companyData %>% group_by (CITY) %>% summarise (groMin = min(PERCENTAGEGROWTH), revMin = min(REVENUE))%>% arrange(desc(groMin))
+
